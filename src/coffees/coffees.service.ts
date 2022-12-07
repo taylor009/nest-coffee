467fb94f1c59b66059dfbe0b6d32b246480bc3a4
Taylor <taylor@baremetalcloud.com>
2022-12-07 18:10:58 -0500

    Added get relationships in get requests

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -12,11 +12,22 @@ export class CoffeesService {
     private readonly coffeeRepository: Repository<Coffee>,
   ) {}
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: {
+        flavors: true,
+      },
+    });
   }
 
   async findOne(id: string) {
-    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: {
+        id: +id,
+      },
+      relations: {
+        flavors: true,
+      },
+    });
 
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
